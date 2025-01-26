2ead18b05375e7ef2b0b7942dea984a95614ca10
tarun <tarunssadhukhan2@gmail.com>
2025-01-26 10:35:49 +0530

    Updated code14dupdate s432

--- inventory/routes.py
+++ inventory/routes.py
@@ -9,7 +9,7 @@ def get_purchase_orders():
     try:
         connection = get_db_connection()
         cursor = connection.cursor(dictionary=True)
-        query = "SELECT * FROM company_master limit 111"
+        query = "SELECT * FROM company_master limit 110"
      
         cursor.execute(query)
         data = cursor.fetchall()
