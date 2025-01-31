8aec159901acb95ec7c2a2f217ed50cdff72f552
tarun <tarunssadhukhan2@gmail.com>
2025-02-01 01:16:51 +0530

    Updated code14dupdate direct podman

--- inventory/routes.py
+++ inventory/routes.py
@@ -9,7 +9,7 @@ def get_purchase_orders():
     try:
         connection = get_db_connection()
         cursor = connection.cursor(dictionary=True)
-        query = "SELECT comp_id,company_name,company_code FROM company_master limit 5"
+        query = "SELECT comp_id,company_name,company_code FROM company_master limit 15"
         cursor.execute(query)
         data = cursor.fetchall()
         cursor.close()
